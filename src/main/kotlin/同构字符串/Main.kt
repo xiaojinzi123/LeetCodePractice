12f2ff9677cad222e01b719b7c00e1363aab9606
xiaojiniz <347837667@qq.com>
2024-06-07 12:57:23 +0800

    同构字符串

--- src/main/kotlin/同构字符串/Main.kt
+++ src/main/kotlin/同构字符串/Main.kt
@@ -9,15 +9,20 @@ private fun isIsomorphic(s1: String, s2: String): Boolean {
     if (s1.isEmpty()) {
         return false
     }
-    val asciiArray = IntArray(256).apply {
+    val asciiArray1 = IntArray(256).apply {
         this.fill(element = -1)
     }
+    val asciiArray2 = BooleanArray(256).apply {
+        this.fill(element = false)
+    }
     for (index in s1.indices) {
-        val arrIndex = s1.elementAt(index = index).code
-        if(asciiArray[arrIndex] == -1) {
-            asciiArray[arrIndex] = s2.elementAt(index = index).code
+        val s1ItemCode = s1.elementAt(index = index).code
+        val s2ItemCode = s2.elementAt(index = index).code
+        if(asciiArray1[s1ItemCode] == -1 && !asciiArray2[s2ItemCode]) {
+            asciiArray1[s1ItemCode] = s2ItemCode
+            asciiArray2[s2ItemCode] = true
         } else {
-            if (asciiArray[arrIndex] != s2.elementAt(index = index).code) {
+            if (asciiArray1[s1ItemCode] != s2ItemCode) {
                 return false
             }
         }
@@ -31,6 +36,7 @@ fun main() {
         ("egg" to "add") to true,
         ("foo" to "bar") to false,
         ("paper" to "title") to true,
+        ("badc" to "baba") to false,
     )
 
     Assert.assertTrue(

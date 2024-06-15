3473fe7c388c2dc9f7919af858565a94863c031b
xiaojiniz <347837667@qq.com>
2024-06-15 20:42:14 +0800

    反转链表(原链表上操作)

--- src/main/kotlin/反转链表/Main.kt
+++ src/main/kotlin/反转链表/Main.kt
@@ -4,6 +4,7 @@ import org.junit.Assert
 import support.LinkNode
 import support.toLinkedList
 
+// 这种是新创建了一个链表
 private fun <T> reverseList1(head: LinkNode<T>): LinkNode<T> {
     // 创建反转后的 Head, 首先是为原始的 Head
     var resultHead = LinkNode(
@@ -20,6 +21,19 @@ private fun <T> reverseList1(head: LinkNode<T>): LinkNode<T> {
     return resultHead
 }
 
+// 不能用另外一个链表, 需要在原链表上进行操作
+private fun <T> reverseList2(head: LinkNode<T>): LinkNode<T> {
+    var parentNode: LinkNode<T>? = null
+    var currentNode: LinkNode<T>? = head
+    while (currentNode != null) {
+        val tempNode = currentNode.nextNode
+        currentNode.nextNode = parentNode
+        parentNode = currentNode
+        currentNode = tempNode
+    }
+    return parentNode!!
+}
+
 fun main() {
 
     val questionAndAnswerList = listOf(
@@ -30,6 +44,8 @@ fun main() {
     Assert.assertTrue(
         questionAndAnswerList.all { item ->
             reverseList1(item.first.toLinkedList()).toListForNext().map { it.value } == item.second
+        } && questionAndAnswerList.all { item ->
+            reverseList2(item.first.toLinkedList()).toListForNext().map { it.value } == item.second
         }
     )
 
